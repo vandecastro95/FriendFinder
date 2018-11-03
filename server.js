e8536c747be21ba2f34eeab20975dc22403ed1c5
unknown <vandecastro95@gmail.com>
2018-11-02 23:29:09 -0500

    added html, css, index.js, server, and routing working

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let path = require('path');
+let bodyParser = require('body-parser')
 
 let PORT = process.env.PORT || 3000;
 let app = express();
 
-app.use(express.urlencoded({extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'app/public/')));
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
